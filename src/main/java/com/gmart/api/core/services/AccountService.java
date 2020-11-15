d4066190227905322cad4fdf2d174e4b25f8ae61
Youssef TOUNOUSSI <youssef.tounoussi@gmail.com>
2020-11-15 02:03:31 +0100

    Add CRUD to account service

--- src/main/java/com/gmart/api/core/services/AccountService.java
+++ src/main/java/com/gmart/api/core/services/AccountService.java
@@ -6,20 +6,50 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
+import com.gmart.api.core.entities.Profile;
 import com.gmart.api.core.entities.UserCore;
 import com.gmart.api.core.repositories.UserRepository;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
-@Service
 @Slf4j
-public class AccountService {
+@Data
+@Service
+public class AccountService implements UserDetailsService {
 	@Autowired
-	UserRepository userRepository;
+	private UserRepository userRepository;
 
+	public UserCore loadUserById(Long id) {
+		return this.userRepository.getOne(id);
+	}
+	
 	public List<UserCore> getAll(){
 		return this.userRepository.findAll();
 	}
+	
+	public List<UserCore> getMatchingAccountList(String criteria){
+		return this.userRepository.findByUsernameContainingIgnoreCase(criteria);
+	}
+	
+	public UserCore loadUserByUsername(String username) {
+		return userRepository.findByUsername(username);
+	}
+	
+	public UserCore loadUserByProfile(Profile profile) {
+		return userRepository.findByProfile(profile);
+	}
+
+	public UserCore save(UserCore user) {
+		return userRepository.saveAndFlush(user);
+	}
+
+	public UserCore update(UserCore user) {
+		return userRepository.save(user);
+	}
+	
 }
