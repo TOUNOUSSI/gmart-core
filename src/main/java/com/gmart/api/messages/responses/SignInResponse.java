7c2e6950c62465107e50bd66c3fc5e3b21d76540
Youssef TOUNOUSSI <youssef.tounoussi@gmail.com>
2020-11-24 01:16:53 +0100

    Add jsonProperty names

--- src/main/java/com/gmart/api/messages/responses/SignInResponse.java
+++ src/main/java/com/gmart/api/messages/responses/SignInResponse.java
@@ -1,13 +1,21 @@
 package com.gmart.api.messages.responses;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.gmart.api.messages.responses.enums.LoginStatus;
 
 import lombok.Data;
 
 @Data
 public class SignInResponse {
+	@JsonProperty("authenticatedUser")
 	UserInfo authenticatedUser;
+	
+	@JsonProperty("error")
 	CustomError error;
+	
+	@JsonProperty("loginStatus")
 	LoginStatus loginStatus;
+	
+	@JsonProperty("token")
 	String token;
 }
