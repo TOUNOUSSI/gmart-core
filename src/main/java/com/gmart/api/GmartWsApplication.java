1dde8fc4e91de4f15fc94bf07ffd9070808e0332
Youssef TOUNOUSSI <youssef.tounoussi@gmail.com>
2021-03-24 01:39:19 +0100

    Enable Eureka client

--- src/main/java/com/gmart/api/GmartWsApplication.java
+++ src/main/java/com/gmart/api/GmartWsApplication.java
@@ -3,7 +3,11 @@ package com.gmart.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableEurekaClient
+@EnableTransactionManagement
 @SpringBootApplication
 @EnableAutoConfiguration
 public class GmartWsApplication {
